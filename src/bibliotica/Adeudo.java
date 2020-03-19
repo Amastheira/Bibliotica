13f5fc4fc540a6a1e5585865d5a42808fc27d92a
WirisLife <62110513+WirisLife@users.noreply.github.com>
2020-03-18 23:33:18 -0600

    Update Adeudo.java

--- src/bibliotica/Adeudo.java
+++ src/bibliotica/Adeudo.java
@@ -5,10 +5,40 @@
  */
 package bibliotica;
 
-/**
- *
- * @author marka
- */
 public class Adeudo {
+    private int dias;
+    private double costo;
+    private int retraso;
+
+    public Adeudo(int dias, double costo, int retraso) {
+        this.dias = dias;
+        this.costo = costo;
+        this.retraso = retraso;
+    }
+
+    public int getDias() {
+        return dias;
+    }
+
+    public void setDias(int dias) {
+        this.dias = dias;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public int getRetraso() {
+        return retraso;
+    }
+
+    public void setRetraso(int retraso) {
+        this.retraso = retraso;
+    }
+    
     
 }
