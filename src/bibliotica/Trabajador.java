0188344c5f9c610d9d26619e2d197aff9a874b1e
WirisLife <62110513+WirisLife@users.noreply.github.com>
2020-03-18 23:37:15 -0600

    Update Trabajador.java

--- src/bibliotica/Trabajador.java
+++ src/bibliotica/Trabajador.java
@@ -9,6 +9,50 @@ package bibliotica;
  *
  * @author marka
  */
-public class Trabajador {
+public class Trabajador extends Persona{
+    private Persona pancho;
+    private int antiguedad;
+    private String id_trabajador;
+    private Boolean seguro;
+
+    public Trabajador(Persona pancho, int antiguedad, String id_trabajador, Boolean seguro) {
+        this.pancho = pancho;
+        this.antiguedad = antiguedad;
+        this.id_trabajador = id_trabajador;
+        this.seguro = seguro;
+    }
+
+    public Persona getPancho() {
+        return pancho;
+    }
+
+    public void setPancho(Persona pancho) {
+        this.pancho = pancho;
+    }
+
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+
+    public void setAntiguedad(int antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+
+    public String getId_trabajador() {
+        return id_trabajador;
+    }
+
+    public void setId_trabajador(String id_trabajador) {
+        this.id_trabajador = id_trabajador;
+    }
+
+    public Boolean getSeguro() {
+        return seguro;
+    }
+
+    public void setSeguro(Boolean seguro) {
+        this.seguro = seguro;
+    }
+    
     
 }
